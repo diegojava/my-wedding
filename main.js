00637802dd03bbc43a25edbcc36447e368f56572
Diego Jaimes <djaimes10@gmail.com>
2024-09-02 07:35:02 -0600

    fix mainjs

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 function decryptInvite() {
   let nombre = document.getElementById('nombre')
-  const urlParams = new URL(window.location);
-  const key = urlParams.pathname.substring(1);
+  const urlParams = new URL(window.location.search);
+  const key = urlParams.get('token');
   const decrypt = atob(key)
   const parseDecrypt = JSON.parse(decrypt)
   nombre.innerText = parseDecrypt.nombre
 }
 
-
 decryptInvite()
-
-//sendInviteWithEncodedID('16112024w1')
