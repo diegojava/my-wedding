651ea36b429b77a5db24f0402dff145af7ba90f9
Diego Jaimes <djaimes10@gmail.com>
2024-09-02 07:38:54 -0600

    fix url search param

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function decryptInvite() {
   let nombre = document.getElementById('nombre')
-  const urlParams = new URL(window.location.search);
+  const urlParams = new URLSearchParams(window.location.search);
   const key = urlParams.get('token');
   const decrypt = atob(key)
   const parseDecrypt = JSON.parse(decrypt)
