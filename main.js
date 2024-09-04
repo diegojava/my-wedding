84038330c40ca821c242dbe9afada44c2e8d0795
Diego Jaimes <djaimes10@gmail.com>
2024-09-03 23:18:29 -0600

    fix decrypt func

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function decryptInvite() {
   let nombre = document.getElementById('nombre')
   const urlParams = new URLSearchParams(window.location.search);
-  const key = urlParams.get('token');
+  const key = urlParams.get('token') .replace(/-/g, '+').replace(/_/g, '/');;
   const decrypt = atob(key)
   const parseDecrypt = JSON.parse(decrypt)
   nombre.innerText = parseDecrypt.nombre
